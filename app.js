09534955e34e2eac3c8495c50fb253fe6fafb016
Lucccccccca <124479805+Lucccccccca@users.noreply.github.com>
2026-01-21 23:03:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 // --- KONFIGURATION ---
 const SUPABASE_URL = "https://epeqhchtatxgninetvid.supabase.co";
-const SUPABASE_KEY = "<Schlüssel hier>";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwZXFoY2h0YXR4Z25pbmV0dmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NTIyNTIsImV4cCI6MjA4NDQyODI1Mn0.5yNc888ypwrAcUGvSZM8CfssRMbcovBFyltkSx6fErA";
 
 // Supabase‑Client Instanz
 let supabaseClient = null;
